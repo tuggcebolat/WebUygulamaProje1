85b8853117e2288d4ec7ff77ca0938866758e720
tuggcebolat <tuggce20@gmail.com>
2023-07-20 23:11:04 +0300

    Add project files.

---
+++ Models/KitapTuru.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUygulamaProje1.Models
+{
+    public class KitapTuru
+    {
+        [Key]    //primary key
+        public int Id{ get; set; }
+        [Required]      //null olamaz
+        public string Ad{ get; set; }
+    }
+}
